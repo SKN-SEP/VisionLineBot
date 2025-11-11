e2dbe1ef5b9183269a58f22880c29169dd744e85
AveePB <mike303.py@gmail.com>
2025-11-11 21:14:57 +0000

    Created computer vision package

---
+++ vision/build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/pi/VisionLineBotSrc/VisionLineBot/vision
+# Build directory: /home/pi/VisionLineBotSrc/VisionLineBot/vision/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
